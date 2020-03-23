362a0f0e54643347698686c7e5b17df0c9ef56c7
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2020-03-23 10:43:29 +0100

    :sparkles: Import GLTF loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './style/main.styl'
 import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+const gltfLoader = new GLTFLoader()
 
 /**
  * Sizes
