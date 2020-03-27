7990a45714941cd69c79f7a5163e704fb7a9a3d4
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2020-03-27 14:21:53 +0100

    :bug: Fix resize

--- src/index.js
+++ src/index.js
@@ -556,7 +556,7 @@ window.addEventListener('resize', () => {
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
 
-    // renderer.setSize(sizes.width, sizes.height)
+    renderer.setSize(sizes.width, sizes.height)
 
     effectComposer.setSize(sizes.width, sizes.height)
 })
